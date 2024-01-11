249a0ce618708e60401e46a41292cde12478e90a
Jessie <140177537+jessie928@users.noreply.github.com>
2024-01-11 13:30:49 +0800

    Update functions.R

--- R/functions.R
+++ R/functions.R
@@ -63,7 +63,7 @@ seurat2anndata <- function(obj, outFile = NULL, assay = "RNA", main_layer = "dat
 
   obs <- .regularise_df(obj@meta.data, drop_single_values = drop_single_values)
 
-  var <- .regularise_df(Seurat::GetAssay(obj, assay = assay)@meta.features, drop_single_values = drop_single_values)
+  var <- .regularise_df(Seurat::GetAssay(obj, assay = assay)@meta.data, drop_single_values = drop_single_values)
 
   obsm <- NULL
   reductions <- names(obj@reductions)
